a5d439484f33a3b6fa57bfcefdaf095e11e4417a
amurati <ahmet.murati@live.com>
2022-03-03 19:56:46 +0100

    add

---
+++ Ora001/main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def print2(name):
+    print_hi(name)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm for Kosova')
+    print("This is good")
+    a = 1
+    b = 3.6
+    c = a/b
+    if b == 3.6:
+        print("b == 3.6")
+    print(c)
+    print2("That is it")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
